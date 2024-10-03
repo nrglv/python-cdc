3ac761c197ebe821784c15ee3019a60a395244b5
Arman Nurgaliyev <nrglv.rmn@gmail.com>
2024-10-03 13:49:35 +0500

    added .env

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -1,17 +1,21 @@
-from __future__ import with_statement
-import logging
-from logging.config import fileConfig
+import os
 from sqlalchemy import engine_from_config, pool
 from alembic import context
-from database import Base  
+from database import Base
+from dotenv import load_dotenv
+
+load_dotenv()
 
 config = context.config
 
-fileConfig(config.config_file_name)
+config.set_main_option('sqlalchemy.url', os.getenv('DATABASE_URL'))
+
+
 
 target_metadata = Base.metadata
 
 def run_migrations_offline():
+    """Run migrations in 'offline' mode."""
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
@@ -24,6 +28,7 @@ def run_migrations_offline():
         context.run_migrations()
 
 def run_migrations_online():
+    """Run migrations in 'online' mode."""
     connectable = engine_from_config(
         config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
